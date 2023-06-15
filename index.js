6607210e80e88b756d312b529b4fdcf9e788cf27
tirth2894 <tirthrollwala@gmail.com>
2023-06-15 18:46:34 +0530

    Local storage feature added.

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+let temp = localStorage.getItem("udata");
+temp = JSON.parse(temp);
+console.log(temp);
+
 const data = [];
+
+for(let i = 0;i<temp.length;i++)
+{
+    let t = {
+        rollno : temp[i].rollno,
+        name : temp[i].name,
+        gender : temp[i].gender,
+        age : temp[i].age,
+        birthdate : temp[i].birthdate 
+    }
+    data.push(t);
+}
+
 let close = document.getElementById("close");
 let addbtn = document.getElementById("addbtn");
 let submit = document.getElementById("submit");
@@ -27,37 +44,39 @@ close.onclick = function () {
     table.style.display = "block";
 }
 
-
 function deletedata() {
-
     let delet = document.getElementsByClassName("delete");
-    for (let i = 0; i < data.length; i++) {
-        delet[i].onclick = function () {
+    for (let x in data) {
+        delet[x].onclick = function () {
             let tr = this.parentElement.parentElement;
             let ind = tr.getAttribute("index");
-            data.splice(ind, 1);
-            tabledata();
+            data.splice(ind,1);
             alert("Data deleted Successfully...");
+            
+            localStorage.setItem("udata",JSON.stringify(data));
+            tabledata();
         }
     }
 }
 
 function tabledata() {
     print.innerHTML = "";
+    
     let index = 0;
     for (let x in data) {
         print.innerHTML +=
-            "<tr index=" + index + "> <td>" + data[x].rollno + "</td> <td>" + data[x].name + "</td> <td>" + data[x].gender + "</td> <td>" + data[x].age + "</td> <td>" + data[x].birthdate +
-            "</td> <td><button class='btn delete'> Delete </button> <button class='btn update'> Update </button></td> </tr>";
+        "<tr index=" + index + "> <td>" + data[x].rollno + "</td> <td>" + data[x].name + "</td> <td>" + data[x].gender + "</td> <td>" + data[x].age + "</td> <td>" + data[x].birthdate +
+        "</td> <td><button class='btn delete'> Delete </button> <button class='btn update'> Update </button></td> </tr>";
         index += 1;
     }
     deletedata();
-    updatedate();
+    updatedata();
 }
+tabledata();
 
-function updatedate() {
+function updatedata() {
     let update = document.getElementsByClassName("update");
-
+    
     for (let i = 0; i < data.length; i++) {
         update[i].onclick = function () {
             let tr = this.parentElement.parentElement;
@@ -66,25 +85,25 @@ function updatedate() {
             let table = document.getElementById("table");
             modal.style.display = "block";
             table.style.display = "none";
-
+            
             let rollno = document.getElementById("u-rollno");
             let name = document.getElementById("u-name");
             let gender = document.getElementById("u-gender");
             let age = document.getElementById("u-age");
             let birthdate = document.getElementById("u-birthdate");
-
+            
             rollno.value = data[index].rollno;
             name.value = data[index].name;
             gender.value = data[index].gender;
             age.value = data[index].age;
             birthdate.value = data[index].birthdate;
-
+            
             u_submit.onclick = function () {
                 let modal = document.getElementById("u-modal");
                 let table = document.getElementById("table");
                 modal.style.display = "none";
                 table.style.display = "block";
-
+                
                 rollno = document.getElementById("u-rollno").value;
                 name = document.getElementById("u-name").value;
                 gender = document.getElementById("u-gender").value;
@@ -103,8 +122,9 @@ function updatedate() {
                         age: age,
                         birthdate: birthdate
                     }
-                    tabledata();
                     alert("Data updated Successfully...");
+                    localStorage.setItem("udata",JSON.stringify(data));
+                    tabledata();
                 }
             }
 
@@ -123,7 +143,7 @@ submit.onclick = function () {
     let gender = document.getElementById("gender").value;
     let age = document.getElementById("age").value;
     let birthdate = document.getElementById("birthdate").value;
-
+    
     if (rollno == "" || name == "" || gender == "" || age == "" || birthdate == "") {
         alert("Please, Enter valid data..")
     }
@@ -135,13 +155,14 @@ submit.onclick = function () {
             age: age,
             birthdate: birthdate
         };
-
+        
         data.push(arr);
         if (data[0] == undefined) {
             alert("Student is not found..");
         }
-        tabledata();
-        alert("Data added Successfully...");
-        form.reset();
+        localStorage.setItem("udata",JSON.stringify(data));
     }
+    
+    form.reset();
+    tabledata();    
 }
